e8f745e301ed76475a15efd8ece63d7adf882bf9
Nazibullah Noyon <nazibullahnoyon19.20@gmail.com>
2024-10-28 09:48:12 +0600

    added

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./email-otp-7a9ff-firebase-adminsdk-c4wy0-d48ced088f.json');
+// const serviceAccount = require('./email-otp-7a9ff-firebase-adminsdk-c4wy0-d48ced088f.json');
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
